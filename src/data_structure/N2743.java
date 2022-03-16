4d0b1e05e3de69dd4d5111c29a0326c95e5b6729
장혜림 <vkfks4472889@gmail.com>
2022-03-16 22:38:19 +0900

    2022-03-16
    백준 - 알파벳 길이

---
+++ src/data_structure/N2743.java
@@ -0,0 +1,13 @@
+package data_structure;
+
+import java.util.Scanner;
+
+public class N2743 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.nextLine();
+		System.out.println(word.length());
+	}
+	
+}
