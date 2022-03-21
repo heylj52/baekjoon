d7c4ac38b7a02f4829ddc4cb2193a04d90fc9302
장혜림 <vkfks4472889@gmail.com>
2022-03-21 23:39:32 +0900

    2022-03-21
    알고리즘 기초 - 브루트포

---
+++ src/brute_force/N1748.java
@@ -0,0 +1,29 @@
+package brute_force;
+
+import java.util.Scanner;
+
+public class N1748 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int result = 0;
+		int last_length = (n+"").length();
+		int last = (int) (n % (Math.pow(10, last_length-1)));
+		int nn = n;
+		//if(n>9 && last == 0) result += last_length;
+		int[] arr = {9, 90*2, 900*3, 9000*4, 90000*5, 900000*6, 9000000*7,90000000*8};
+		while(n/10!=0) {
+			String str = n+"";
+			result += arr[str.length()-2];
+			n = (int) (n % (Math.pow(10, str.length()-1)));
+		}
+		if(nn<10) {
+			result += n*last_length;
+		}else {
+			result += last*last_length + 1*last_length;
+		}
+		System.out.println(result);
+	}
+	
+}
