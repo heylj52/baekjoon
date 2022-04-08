2289bb3fa312ce1bcb04f41652af2f675318adf9
장혜림 <vkfks4472889@gmail.com>
2022-04-08 23:43:41 +0900

    2022-04-08
    알고리즘 기초 - 그래프

--- src/graph/N2606.java
+++ src/graph/N2606.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class N2606 {
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); 
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
