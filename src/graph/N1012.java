4668b57d2b9affd04389903f9eba7e849090624d
장혜림 <vkfks4472889@gmail.com>
2022-04-12 23:12:36 +0900

    2022-04-12

--- src/graph/N1012.java
+++ src/graph/N1012.java
@@ -11,7 +11,7 @@ public class N1012 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
-		while(size-- > 0) {
+		while(size-- > 0) { 
 			int M = sc.nextInt();
 			int N = sc.nextInt();
 			int K = sc.nextInt();
