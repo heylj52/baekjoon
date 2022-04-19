a060c860fb3de141f4f23aa46e9aa538466d5b5d
장혜림 <vkfks4472889@gmail.com>
2022-04-19 23:32:54 +0900

    2022-04-19

--- src/graph/N16929.java
+++ src/graph/N16929.java
@@ -3,6 +3,14 @@ package graph;
 import java.util.Scanner;
 
 public class N16929 {
+	
+	public static int[] dx = {-1, 1, 0, 0};
+	public static int[] dy = {0, 0, -1, 1};
+	
+	public static void dfs(char[][] graph, int i, int j, int N, int M, boolean[][] visit) {
+		char word = graph[i][j];
+		visit[i][j] = true;
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -14,6 +22,12 @@ public class N16929 {
 			String str = sc.nextLine();
 			for(int j=0; j<M; j++) {
 				graph[i][j] = str.charAt(j);
+			}
+		}
+		boolean[][] visit = new boolean[N][M];
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				dfs(graph, i, j, N, M, visit);
 			}
 		}
 	}
