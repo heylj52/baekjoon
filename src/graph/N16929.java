b9a8076fc6bb6121a637bcd461d0f1e42a2733b9
장혜림 <vkfks4472889@gmail.com>
2022-04-22 23:26:46 +0900

    2022-04-22
    알고리즘 기초 - 그래프
    Two Dots Dist로 거리나타내기

--- src/graph/N16929.java
+++ src/graph/N16929.java
@@ -4,40 +4,59 @@ import java.util.Scanner;
 
 public class N16929 {
 	
+	static char[][] graph;
+	static boolean[][] visit;
+	static int[][] dist;
+	static int N;
+	static int M;
 	public static int[] dx = {-1, 1, 0, 0};
 	public static int[] dy = {0, 0, -1, 1};
 	
-	public static void dfs(char[][] graph, int i, int j, int N, int M, boolean[][] visit) {
-		char word = graph[i][j];
-		visit[i][j] = true;
-		for(int x=0; x<4; x++) {
-			int nx = i + dx[x];
-			int ny = j + dy[x];
-			
-			if(graph[nx][ny]==word) { 
-				 
+	public static boolean dfs(int i, int j, int cnt, char color) {
+		if(visit[i][j]) {
+			if(cnt-dist[i][j]>=4) {
+				return true;
+			}else {
+				return false;
 			}
 		}
+		visit[i][j] = true;
+		dist[i][j] = cnt;
+		for(int n=0; n<4; n++) {
+			int nx = i + dx[n];
+			int ny = j + dy[n];
+			if(0<=nx && nx<N && 0<=ny && ny<M) {
+				if(graph[nx][ny]==color) { 
+					 if(dfs(nx, ny, cnt+1, color)) return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		N = sc.nextInt();
+		M = sc.nextInt();
 		sc.nextLine();
-		char[][] graph = new char[N][M];
+		graph = new char[N][M];
 		for(int i=0; i<N; i++) {
 			String str = sc.nextLine();
 			for(int j=0; j<M; j++) {
 				graph[i][j] = str.charAt(j);
 			}
 		}
-		boolean[][] visit = new boolean[N][M];
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<M; j++) {
-				dfs(graph, i, j, N, M, visit);
+				visit = new boolean[N][M];
+				dist = new int[N][M];
+				if(dfs(i, j, 1, graph[i][j])) {
+					System.out.println("Yes");
+					System.exit(0);
+				}
 			}
 		}
+		System.out.println("No");
 	}
 	
 }
